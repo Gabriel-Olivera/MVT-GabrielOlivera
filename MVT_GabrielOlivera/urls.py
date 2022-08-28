e7958dbea63a660eb754713eef4db1469f72abe6
gabrielolivera <gabriel.olivera.surraco@gmail.com>
2022-08-28 12:45:44 -0300

    MVT_GabrielOlivera

--- MVT_GabrielOlivera/urls.py
+++ MVT_GabrielOlivera/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from AppFamiliares.views import familiares, inicio
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', inicio),
